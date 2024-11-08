95df7e6dbebe0c49b4dc5dfa41c343a87ec19b21
Vivek-raj-gupta-2002 <vivekrajgupta2002@outlook.com>
2024-11-08 21:55:00 +0530

    update media in urls

--- siteFiles/urls.py
+++ siteFiles/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path
+from  django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('siteApp.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ]
 
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
